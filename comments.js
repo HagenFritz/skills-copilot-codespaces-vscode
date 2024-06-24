c9bf52488e2b3aecbe0bf62dd8e78358a4c0ddf2
Hagen Fritz <33231914+HagenFritz@users.noreply.github.com>
2024-06-24 19:24:36 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,78 +1,55 @@
 // create web server
-var express = require('express');
-var app = express();
+var http = require('http');
 var fs = require('fs');
-var bodyParser = require('body-parser');
-var multer  = require('multer');
-var upload = multer({dest: 'uploads/'});
+var url = require('url');
+var qs = require('querystring');
+var comments = [];
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+var server = http.createServer(function(req, res){
+    console.log(req.method);
+    var parseUrl = url.parse(req.url, true);
+    var pathname = parseUrl.pathname;
+    if(pathname === '/'){
+        fs.readFile('./views/index.html', function(err, data){
+            if(err){
+                res.end('404 Not Found');
+            }
+            res.end(data);
+        });
+    }else if(pathname === '/post'){
+        fs.readFile('./views/post.html', function(err, data){
+            if(err){
+                res.end('404 Not Found');
+            }
+            res.end(data);
+        });
+    }else if(pathname === '/comment'){
+        if(req.method === 'POST'){
+            var str = '';
+            req.on('data', function(chunk){
+                str += chunk;
+            });
 
-// create a comment
-app.post('/', function(req, res) {
-  var comment = req.body.comment;
-  var user = req.body.user;
-  var date = new Date();
-  var newComment = {
-    comment: comment,
-    user: user,
-    date: date
-  };
-  fs.readFile('./comments.json', 'utf8', function(err, data) {
-    var comments;
-    if (err) {
-      comments = [];
-    } else {
-      comments = JSON.parse(data);
+            req.on('end', function(){
+                var comment = qs.parse(str);
+                comments.push(comment);
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                res.end();
+            });
+        }
+    }else if(pathname === '/comments'){
+        var commentsStr = JSON.stringify(comments);
+        res.end(commentsStr);
+    }else if(pathname.indexOf('/public') === 0){
+        fs.readFile('.' + pathname, function(err, data){
+            res.end(data);
+        });
+    }else{
+        res.end('404 Not Found');
     }
-    comments.push(newComment);
-    fs.writeFile('./comments.json', JSON.stringify(comments), function(err) {
-      if (err) {
-        res.status(500).send('Could not save comment');
-      } else {
-        res.status(201).send(newComment);
-      }
-    });
-  });
 });
 
-// get all comments
-app.get('/', function(req, res) {
-  fs.readFile('./comments.json', 'utf8', function(err, data) {
-    if (err) {
-      res.status(500).send('Could not read comments');
-    } else {
-      res.status(200).send(data);
-    }
-  });
+server.listen(3000, function(){
+    console.log('Server is running on port 3000');
 });
-
-// get comment by id
-app.get('/:id', function(req, res) {
-  var id = req.params.id;
-  fs.readFile('./comments.json', 'utf8', function(err, data) {
-    if (err) {
-      res.status(500).send('Could not read comments');
-    } else {
-      var comments = JSON.parse(data);
-      var comment = comments[id];
-      if (comment) {
-        res.status(200).send(comment);
-      } else {
-        res.status(404).send('Comment not found');
-      }
-    }
-  });
-});
-
-// update comment by id
-app.put('/:id', function(req, res) {
-  var id = req.params.id;
-  var newComment = req.body.comment;
-  fs.readFile('./comments.json', 'utf8', function(err, data) {
-    if (err) {
-      res.status(500).send('Could not read comments');
-    } else {
-      var comments = JSON.parse(data);
-      var comment =
